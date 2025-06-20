d547551fb268b3f3f8ae4a614f6dd4fe6f792a11
MGT-21 <mgt21inv@gmail.com>
2025-06-20 18:05:57 -0300

    :sparkles: feat: Página de cadastro

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,29 +1,23 @@
 import Input from "../components/Input"
-import Checkbox from "../components/Checkbox"
 import Button from "../components/Button"
 
-import './LoginPage.css'
-
-
-export default function LoginPage() {
+export default function RegisterPage() {
 
     return (
         <div className="row h-100 ">
             <div className="ImageContainer d-none d-md-block col-md-8"></div>
             <div className="d-flex justify-content-center flex-column col-md-4 bg-custom">
-                <h1 className="text-center mb-4 fw-bold">Login</h1>
+                <h1 className="text-center mb-4 fw-bold">Cadastro</h1>
                 <form className="px-3">
+                    <Input type="name" label="Nome" placeholder="Digite seu nome" />
                     <Input type="email" label="E-mail" placeholder="Digite seu email" />
                     <Input type="password" label="Senha" placeholder="Digite sua Senha" />
-                    <Checkbox label="Lembre-se de mim" />
-                    <div class="d-grid gap-3 mt-5">
-                        <Button type="submit" text="Entrar" />
-                        <Button type="button" text="Cadastrar-se" href="/register" />
+                    <div className="d-grid gap-3 mt-5">
+                        <Button type="submit" text="Cadastrar-se" />
                     </div>
-                    <a href="" className="d-block text-center mt-3 text-white">Esqueci minha Senha</a>
+                    <a href="/login" className="d-block text-center mt-3 text-white">Já tenho uma Conta</a>
                 </form>
             </div>
         </div>
     )
 }
-
