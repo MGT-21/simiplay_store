709835f5ff31af49b416835ff4a9f5f98f1f4057
MGT-21 <mgt21inv@gmail.com>
2025-06-20 17:43:25 -0300

    :lipstick: feat: Variáveis Css aplicadas aos inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,11 @@
+import './Input.css'
+
 export default function Input({type,label,placeholder,id}) {
 
     return (
         <div className="mb-3">
             <label for="exampleInputEmail1" className="form-label fw-medium">{label}</label>
-            <input type={type} className="form-control" id={id} aria-describedby="emailHelp" placeholder={placeholder}/>
+            <input type={type} className="form-control custom-input" id={id} aria-describedby="emailHelp" placeholder={placeholder}/>
         </div>
     )
 }
