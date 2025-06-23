888b28a377ef7473cce2a89cc62a731f1078bd76
MGT-21 <mgt21inv@gmail.com>
2025-06-23 15:05:41 -0300

    :broom: cleanup: Eliminando código do botão cadastrar-se

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,10 @@
 import './Button.css'
 
-export default function Button({type,text,href,id}) {
+export default function Button({type,text,id}) {
 
-        if (href == undefined){
-            return (
-                <button className="btn fw-bold btn-custom text-black w-100" type={type} id={id}>
-                    {text} 
-                </button>
-         )}else {
-            return (
-                <a className="btn btn-outline-custom fw-bold text-custom w-100" type={type} href={href}>
-                    {text}
-                </a>
-         )}
+        return (
+            <button className="btn fw-bold btn-custom text-black w-100" type={type} id={id}>
+                {text} 
+            </button>
+        )  
 }
