1853977a5553c19f9aa268a4b3db7cc922ac2b97
MGT-21 <mgt21inv@gmail.com>
2025-07-13 16:51:48 -0300

    :sparkles: feat: implementação do react-hook-form no formulário de login

--- src/layout/LoginForm/index.jsx
+++ src/layout/LoginForm/index.jsx
@@ -1,3 +1,5 @@
+import { useForm } from "react-hook-form";
+
 import Input from "../../components/Input"
 import Checkbox from "../../components/Checkbox"
 import Button from "../../components/Button"
@@ -6,16 +8,32 @@ import './index.css'
 
 export default function LoginForm() {
 
-    return(
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log("Dados recebidos:", data);
+    };
+
+    return (
         <div className="d-flex justify-content-center flex-column col-md-5 bg-custom px-3">
             <h1 className="text-center mb-4 fw-bold">Login</h1>
-            <form className="">
-                <Input type="email" label="E-mail" placeholder="Digite seu email" />
-                
-                <Input type="password" label="Senha" placeholder="Digite sua Senha" />
-                
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    type="email"
+                    label="E-mail"
+                    placeholder="Digite seu email"
+                    {...register("email")}
+                />
+
+                <Input
+                    type="password"
+                    label="Senha"
+                    placeholder="Digite sua Senha"
+                    {...register("password")}
+                />
+
                 <Checkbox label="Lembre-se de mim" />
-                
+
                 <Button type="submit" text="Entrar" />
             </form>
             <p className="d-block text-center mt-3 text-white fw-medium">Não tem conta ? <a className="link-danger" href="/register">Registrar-se</a></p>
