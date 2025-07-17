1e524a2766af554ca3977605698ac73da26e15f1
MGT-21 <mgt21inv@gmail.com>
2025-07-17 16:58:29 -0300

    :recycle: refactor: Declaração de classe no componente Checkbox

--- client/src/components/Checkbox/index.jsx
+++ client/src/components/Checkbox/index.jsx
@@ -3,7 +3,7 @@ import './index.css'
 export default function Checkbox({label,id}) {
 
     return (
-        <div class="mb-3 form-check">
+        <div className="mb-3 form-check">
             <input type="checkbox" className="form-check-input custom-check" id={id}/>
             <label className="form-check-label">{label}</label>
         </div>
