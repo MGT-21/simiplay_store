e6c1828432d6b8a217eabb004dc2e66abd8e44a0
MGT-21 <mgt21inv@gmail.com>
2025-08-07 03:33:36 -0300

    :sparkles: feat: Ajustes de layout no banner mobile

--- client/src/components/home/HomeBanner.tsx
+++ client/src/components/home/HomeBanner.tsx
@@ -1,5 +1,9 @@
 import Banner1 from "@/assets/banner-1.png";
 
+import * as React from "react";
+
+import { type CarouselApi } from "@/components/ui/carousel";
+
 import {
   Carousel,
   CarouselContent,
@@ -9,35 +13,69 @@ import {
 } from "@/components/ui/carousel";
 
 export default function HomeBanner() {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
-    <div className="max-w-[1200px] mx-auto">
-      <Carousel className="">
+    <div className="max-w-[400px] mx-auto mt-12">
+      <Carousel className="" setApi={setApi}>
         <CarouselContent className="">
           <CarouselItem className="">
-            <img
-              src={Banner1}
-              alt="Promoção"
-              className=" h-auto object-cover"
-            />
+            <div className="px-5">
+              <img
+                src={Banner1}
+                alt="Promoção"
+                className=" h-auto object-cover rounded-md"
+              />
+            </div>
           </CarouselItem>
           <CarouselItem className="">
-            <img
-              src={Banner1}
-              alt="Promoção"
-              className=" h-auto object-cover"
-            />
+            <div className="px-5">
+              <img
+                src={Banner1}
+                alt="Promoção"
+                className=" h-auto object-cover rounded-md"
+              />
+            </div>
           </CarouselItem>
           <CarouselItem className="">
-            <img
-              src={Banner1}
-              alt="Promoção"
-              className=" h-auto object-cover"
-            />
+            <div className="px-5">
+              <img
+                src={Banner1}
+                alt="Promoção"
+                className=" h-auto object-cover rounded-md"
+              />
+            </div>
           </CarouselItem>
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <div className="relative">
+          <CarouselPrevious className=" left-30 top-6" />
+          <CarouselNext className=" right-30 top-6" />
+        </div>
       </Carousel>
+      <div className="flex justify-center gap-2 mt-5 items-center">
+        {Array.from({ length: count }).map((_, index) => (
+          <span
+            key={index}
+            className={`h-2 w-2 flex rounded-full transition-all duration-300 ${current === index + 1 ? 'bg-blue-500 w-4 h-2' : 'bg-gray-400'
+              }`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- client/src/components/home/HomeContent.tsx
+++ client/src/components/home/HomeContent.tsx
@@ -3,9 +3,8 @@
 export default function HomeContent() {
 
   return (
-   <section className="mt-8">
-      <h2 className="text-xl font-bold">Produtos em destaque</h2>
-      {/* Lista de produtos */}
+   <section className="mt-12">
+      
     </section>
   );
 }
