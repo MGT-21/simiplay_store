5b72a7105d241dd81114cef9a3851c01eee2e8bb
MGT-21 <mgt21inv@gmail.com>
2025-08-14 10:14:25 -0300

    :recycle: refactor: Ajuste no espaçamento e tamanho do componente de pesquisa do header xl

--- client/src/components/header/SearchHeader.tsx
+++ client/src/components/header/SearchHeader.tsx
@@ -8,7 +8,7 @@ interface SearchHeaderProps {
 
 export default function SearchHeader({ onClose }: SearchHeaderProps) {
   return (
-    <div className="fixed top-0 left-0 w-full z-50 flex items-center gap-3 bg-zinc-900 px-3 py-2">
+    <div className="fixed top-0 left-0 w-full z-50 flex items-center gap-3 xl:gap-8 xl:px-6 bg-zinc-900 px-3 py-2 xl:py-3.25">
       <button onClick={onClose} className="p-2 rounded">
         <ArrowLeft className="size-5.5"/>
       </button>
